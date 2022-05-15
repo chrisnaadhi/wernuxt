89a8f0449a964a32c5059535b8ee79aa480c5a6f
chrisnaadhi <chrisnaadhip@gmail.com>
2022-05-15 16:36:57 +0700

    fix clock and add toggle dark

--- app.vue
+++ app.vue
@@ -1,29 +1,35 @@
 <script setup>
-import "./assets/styles/style.css";
+import './assets/styles/style.css';
+
+const toggleDark = () => {
+  document.body.classList.toggle('dark');
+};
 </script>
 
 <template>
-  <div class="text-center mt-5">
-    <!-- <NuxtWelcome /> -->
-    <h1 class="heading">Hello World</h1>
-    <p class="font-bold text-9xl text-gray-800">
-      Nuxt<span class="nuxt-color">3</span>
-    </p>
-    <img
-      class="logo"
-      src="https://avatars.githubusercontent.com/u/23360933"
-      alt=""
-    />
-  </div>
-  <footer>
-    <Credit />
-  </footer>
+  <main class="dark:bg-gray-600">
+    <div class="text-center mt-5">
+      <!-- <NuxtWelcome /> -->
+      <h1 class="heading text-4xl">Hello World</h1>
+      <p class="font-bold text-7xl text-gray-800 dark:text-white">
+        Nuxt<span class="nuxt-color">3</span>
+      </p>
+      <img
+        class="logo"
+        src="https://avatars.githubusercontent.com/u/23360933"
+        alt=""
+      />
+    </div>
+    <footer class="flex flex-col items-center justify-center dark:text-white">
+      <button @click="toggleDark">Toggle Dark</button>
+      <Credit />
+    </footer>
+  </main>
 </template>
 
 <style>
 .heading {
-  color: #42b883;
-  @apply text-5xl;
+  color: #00dc82;
 }
 
 .nuxt-color {
@@ -31,6 +37,6 @@ import "./assets/styles/style.css";
 }
 
 .logo {
-  @apply w-56 rounded-xl mt-5;
+  @apply w-36 rounded-xl my-6;
 }
 </style>
--- components/Credit.vue
+++ components/Credit.vue
@@ -1,8 +1,19 @@
 <script setup>
-const myName = ref("Chrisna Adhi Pranoto");
-// const { currentTime } = useCurrentTime();
+const myName = ref('Chrisna Adhi Pranoto');
+const { currentTime } = useCurrentTime();
+
+const options = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+};
 </script>
 
 <template>
+  <p class="text-xl">
+    {{ currentTime.toLocaleDateString('id-ID', options) }} |
+    {{ currentTime.toLocaleTimeString('id-ID') }}
+  </p>
   <p>&copy; {{ myName }} | Nuxt 3 + UnoCSS</p>
 </template>
