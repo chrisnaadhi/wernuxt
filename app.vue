5c5bc3791b490d1fbad77f16ee013bc734977713
Chrisna Adhi Pranoto <chrisnaadhip@gmail.com>
2022-05-17 11:45:51 +0700

    add variant-group pkg

--- app.vue
+++ app.vue
@@ -2,7 +2,10 @@
 import { ref } from "vue";
 import "./assets/styles/style.css";
 
+useHead;
+
 const isDark = ref(false);
+const userName = ref("World");
 const toggleDark = () => {
   isDark.value = !isDark.value;
   document.body.classList.toggle("dark");
@@ -10,10 +13,18 @@ const toggleDark = () => {
 </script>
 
 <template>
-  <main class="h-4xl py-5 dark:bg-gray-600">
+  <Head>
+    <Title>Hello {{ userName }}</Title>
+    <Link
+      rel="icon"
+      href="https://nuxtjs.org/_nuxt/img/logo.d0868f5.svg"
+      type="image/x-icon"
+    />
+  </Head>
+  <main class="h-screen py-5 dark:(bg-gray-800 text-white)">
     <div class="text-center">
       <!-- <NuxtWelcome /> -->
-      <h1 class="nuxt-color text-4xl">Hello World</h1>
+      <h1 class="nuxt-color text-4xl">Hello, {{ userName }}</h1>
       <p class="font-bold text-7xl text-gray-800 dark:text-white">
         Nuxt<span class="nuxt-color">3</span>
       </p>
@@ -32,12 +43,16 @@ const toggleDark = () => {
       />
     </div>
     <footer class="flex flex-col items-center justify-center">
-      <p class="mb-5 dark:text-white">Toggle Theme :</p>
+      <p class="mb-5">Toggle Theme :</p>
       <div @click="toggleDark" class="dark-toggle">
         <span v-if="!isDark">🌙</span>
         <span v-else>☀️</span>
       </div>
       <Credit />
+      <div>
+        Change to your name :
+        <input type="text" v-model="userName" class="mt-5" />
+      </div>
     </footer>
   </main>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,12 @@
 import { defineNuxtConfig } from "nuxt";
 import transformerDirective from "@unocss/transformer-directives";
+import transformerVariantGroup from "@unocss/transformer-variant-group";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ["@unocss/nuxt"],
   unocss: {
     uno: true,
-    transformers: [transformerDirective()],
+    transformers: [transformerDirective(), transformerVariantGroup()],
   },
 });
